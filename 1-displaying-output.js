a3aa02b4bc22540604310c432f7ce035dcde953a
Brian Alsup <Brian@BetamoreAdmins-MacBook-Air-5.local>
2022-03-09 19:19:31 -0500

    Array of Sunshine

--- 1-displaying-output.js
+++ 1-displaying-output.js
@@ -1,7 +1,7 @@
 //The song is called 'Fuckin Problems' by Asap Rocky
 console.log("I love bad bitches that's my fuckin problem")
 
-let age = 24
+let age = 29
 console.log(age)
 
 console.log("I would like to thank\nmy arms for always being\nby my side, my legs\nfor always supporting me, and\nmy fingers, because I can\nalways count on them.")
---
+++ 4-arrays.js
@@ -0,0 +1,13 @@
+let fruits =["Grapes", "Banana", "Kiwi", "Lime", "Grapefruit", "Avacado"]
+
+console.log(fruits.length)
+
+console.log(fruits[3])
+
+fruits[1] = "Blueberry"
+
+console.log(fruits)
+
+let firstFruit = fruits.pop()
+
+console.log(firstFruit)
