66fa268f4c2f5c3962f35075ed203ac7554172f6
Brian Alsup <Brian@BetamoreAdmins-MacBook-Air-5.local>
2022-03-09 20:30:56 -0500

    On one condition

---
+++ 7-functions.js
@@ -0,0 +1,17 @@
+function howLong(str, num) {
+    if (str.length > num) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function callHowLong() {
+    return howLong("how are you", 4)
+}
+
+//console.log(howLong("how are you", 4))
+
+console.log(callHowLong())
+
+
